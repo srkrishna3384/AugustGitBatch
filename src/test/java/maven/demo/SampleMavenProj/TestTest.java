25016a84311fcd8097962a57d3fc8e920ce65200
RK <RK@DESKTOP-4334PHR.testingxperts.loc>
2020-05-11 17:06:13 +0530

    Asser.fail

--- src/test/java/maven/demo/SampleMavenProj/TestTest.java
+++ src/test/java/maven/demo/SampleMavenProj/TestTest.java
@@ -1,10 +1,12 @@
 package maven.demo.SampleMavenProj;
 
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
 	@Test
 public void test() {
+		Assert.fail();
 	System.out.println("TestTest Class");
 	System.out.println("");
 	System.out.println("");
