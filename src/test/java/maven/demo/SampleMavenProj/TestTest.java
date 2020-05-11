fb2647e0279ee117d25f61eba22003beff4d28c1
RK <RK@DESKTOP-4334PHR.testingxperts.loc>
2020-05-11 17:10:27 +0530

    pass

--- src/test/java/maven/demo/SampleMavenProj/TestTest.java
+++ src/test/java/maven/demo/SampleMavenProj/TestTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 public class TestTest {
 	@Test
 public void test() {
-		Assert.fail();
 	System.out.println("TestTest Class");
 	System.out.println("");
 	System.out.println("");
