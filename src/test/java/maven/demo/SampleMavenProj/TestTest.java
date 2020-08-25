fcb3ff837d8bb3f446557d5c1b9e88f982281c91
srkrishna3384 <srkrishna3384@gmail.com>
2020-08-25 09:00:50 +0530

    first commit

--- src/test/java/maven/demo/SampleMavenProj/TestTest.java
+++ src/test/java/maven/demo/SampleMavenProj/TestTest.java
@@ -1,13 +1,29 @@
 package maven.demo.SampleMavenProj;
 
-import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
 	@Test
 public void test() {
-	System.out.println("TestTest Class");
-	System.out.println("");
-	System.out.println("");
+		
+		System.out.println("TestTest Class");
+		System.out.println("");
+		System.out.println("");
+		
+		
+		
+		
+//		  //setup the chromedriver using WebDriverManager
+//        WebDriverManager.chromedriver().setup();
+// 
+//        //Create driver object for Chrome
+//        WebDriver driver = new ChromeDriver();
+// System.out.println("Browser Launched....");
+//        //Navigate to a URL
+//        driver.get("https://www.rameshsoft.com");
+// System.out.println("Navigated to Rameshsoft website....");
+//        //quit the browser
+//        driver.quit();
+//        System.out.println("Closed Browser.....");
 }
 }
